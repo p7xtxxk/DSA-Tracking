19ee696685975e11cd86b493c9c507aa9ae8a2b7
Prateek Priyanshu <work.prateekpriyanshu@gmail.com>
2025-11-27 21:09:08 +0530

    Comparator and Comparable Rev

--- Revision/Day1_arraylist.java
+++ Revision/Day1_arraylist.java
@@ -3,6 +3,7 @@ import java.util.List;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashSet;
 
 public class Day1_arraylist {
     public static void nain(String args[]) throws IOException {
@@ -10,9 +11,9 @@ public class Day1_arraylist {
         // basic
         List<Integer> list1 = new ArrayList<Integer>();
         // non modifiable at all
-        List<Integer> list2 = List.of(1,2,3,4,5,6);
+        List<Integer> list2 = List.of(1, 2, 3, 4, 5, 6);
         // fixed size; element modification only
-        List<Integer> list3 = Arrays.asList(1,2,3,4,5,6,677,8,8,8);
+        List<Integer> list3 = Arrays.asList(1, 2, 3, 4, 5, 6, 677, 8, 8, 8);
         // initial capacity: size is 1.5x once the capacity is full
         List<Integer> list4 = new ArrayList<Integer>(20);
         // collection
@@ -34,7 +35,7 @@ public class Day1_arraylist {
         // remove element
         list1.remove(Integer.valueOf(3));
         // change the value at index
-        list1.set(3,55);
+        list1.set(3, 55);
         // find the first index of any element
         list1.indexOf(4);
         // find the last index of any element
@@ -42,13 +43,16 @@ public class Day1_arraylist {
         // empty check
         list1.isEmpty();
         // clear the list
-        list1.clear(); 
+        list1.clear();
         // sort by comparator
-        Comparator<Integer> comp = new Comparator<Integer>(){
+        Comparator<Integer> comp = new Comparator<Integer>() {
             public int compare(Integer a, Integer b) {
-                if (a>b) return 1;
-                else if (a==b) return 0;
-                else return -1;
+                if (a > b)
+                    return 1;
+                else if (a == b)
+                    return 0;
+                else
+                    return -1;
             }
         };
         list1.sort(comp);
@@ -56,4 +60,39 @@ public class Day1_arraylist {
         String[] arr1 = list1.toArray(new String[0]);
         Integer[] arr2 = list1.toArray(new Integer[0]);
     }
+
+    public ArrayList<Integer> findDuplicates(int[] arr) {
+        HashSet<Integer> checked = new HashSet<Integer>();
+        ArrayList<Integer> duplicates = new ArrayList<Integer>();
+
+        for (int x : arr) {
+            boolean flag = checked.add(x);
+            if (!flag)
+                duplicates.add(x);
+        }
+
+        return duplicates;
+    }
 }
+
+// User function Template for Java
+
+class Solution {
+    static void reArrange(int[] arr, int N) {
+        ArrayList<Integer> list = new ArrayList<Integer>(N);
+        int lastEven = 0, lastOdd = 1;
+        for (int x : arr) {
+            list.add(x);
+        }
+        for(int x: list) {
+            if(x%2==0){
+                arr[lastEven] = x;
+                lastEven+=2;
+            }
+            else {
+                arr[lastOdd] = x;
+                lastOdd += 2;
+            }
+        }
+    }
+};
---
+++ Revision/Day2_Comparator.java
@@ -0,0 +1,85 @@
+import java.io.*;
+import java.util.*;
+
+public class Day2_Comparator {
+    public static void main(String args[]) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Student> list = new ArrayList<Student>();
+        list.add(new Student(22, "Prateek", 33));
+        list.add(new Student(19, "Ryu", 554));
+        list.add(new Student(25, "Yogi",234));
+        list.add(new Student(21, "Amit", 72));
+
+        // separate class implementation
+        Collections.sort(list, new SortByRollAsc());
+        Collections.sort(list, new SortByRollDesc());
+        Collections.sort(list, new SortByNameLengthDesc());
+        Collections.sort(list, new SortByNameLengthAsc());
+        Collections.sort(list, new SortByAgeDesc());
+        Collections.sort(list, new SortByAgeAsc());
+
+
+        // anonymous class impleemntation
+        Comparator<Student> SortByRollAsc = new Comparator<Student>() {
+            public int compare(Student a, Student b) {
+                return a.roll - b.roll;
+            }
+        };
+        Comparator<Student> SortByNameLengthDesc = new Comparator<Student>() {
+            public int compare(Student a, Student b) {
+                return b.name.length() - a.name.length();
+            }
+        };
+
+        Collections.sort(list, SortByRollAsc);
+        Collections.sort(list, SortByNameLengthDesc);
+    }
+}
+
+class Student {
+    int age;
+    int roll;
+    String name;
+
+    Student(int age, String name, int roll) {
+        this.age = age;
+        this.roll = roll;
+        this.name = name;
+    }
+}
+
+class SortByRollAsc implements Comparator<Student> {
+    public int compare(Student a, Student b) {
+        return a.roll - b.roll;
+    }
+}
+
+class SortByRollDesc implements Comparator<Student> {
+    public int compare(Student a, Student b) {
+        return b.roll - a.roll;
+    }
+}
+
+class SortByNameLengthDesc implements Comparator<Student> {
+    public int compare(Student a, Student b) {
+        return b.name.length() - a.name.length();
+    }
+}
+
+class SortByNameLengthAsc implements Comparator<Student> {
+    public int compare(Student a, Student b) {
+        return a.name.length() - b.name.length();
+    }
+}
+
+class SortByAgeDesc implements Comparator<Student> {
+    public int compare(Student a, Student b) {
+        return b.age - a.age;
+    }
+}
+
+class SortByAgeAsc implements Comparator<Student> {
+    public int compare(Student a, Student b) {
+        return a.age - b.age;
+    }
+}
